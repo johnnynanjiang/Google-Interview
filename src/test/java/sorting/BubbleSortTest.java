d0a57c8d026d01e663cc64f1f56dd6e57a4365e1
Nan Jiang <johnny.nan.jiang@gmail.com>
2017-11-18 08:37:58 +1100

    refactoring

--- src/test/java/sorting/BubbleSortTest.java
+++ src/test/java/sorting/BubbleSortTest.java
@@ -1,5 +1,6 @@
 package sorting;
 
+import fixture.Fixtures;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -10,10 +11,11 @@ import static org.junit.Assert.*;
  * Created by nanjiang on 17/11/17.
  */
 public class BubbleSortTest {
-    final int[] array = {5, 4, 6, 7, 1, 2, 3};
-
     @Test
     public void testBubbleSortUp() {
-        assertEquals("[1, 2, 3, 4, 5, 6, 7]", Arrays.toString(BubbleSort.sortUp(array)));
+        assertEquals(
+                Fixtures.RESULT_OF_SORT_UP,
+                Arrays.toString(BubbleSort.sortUp(Fixtures.ARRAY_TO_SORT))
+        );
     }
 }
--- src/test/java/sorting/InsertionSortTest.java
+++ src/test/java/sorting/InsertionSortTest.java
@@ -1,5 +1,6 @@
 package sorting;
 
+import fixture.Fixtures;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -9,21 +10,19 @@ import java.util.Arrays;
  * Created by nanjiang on 15/11/17.
  */
 public class InsertionSortTest {
-    final int[] array = {5, 4, 6, 7, 1, 2, 3};
-
     @Test
     public void testInsertionSortUp() {
         assertEquals(
-                "[1, 2, 3, 4, 5, 6, 7]",
-                Arrays.toString(InsertionSort.sortUp(array))
+                Fixtures.RESULT_OF_SORT_UP,
+                Arrays.toString(InsertionSort.sortUp(Fixtures.ARRAY_TO_SORT))
         );
     }
 
     @Test
     public void testInsertionSortDown() {
         assertEquals(
-                "[7, 6, 5, 4, 3, 2, 1]",
-                Arrays.toString(InsertionSort.sortDown(array))
+                Fixtures.RESULT_OF_SORT_DOWN,
+                Arrays.toString(InsertionSort.sortDown(Fixtures.ARRAY_TO_SORT))
         );
     }
 }
