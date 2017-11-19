5309a085a91cfb574aab73e60fe9ccf1a223fbb7
Nan Jiang <johnny.nan.jiang@gmail.com>
2017-11-19 21:59:25 +1100

    add report class

---
+++ src/main/java/reporting/SortingAlgorithmReport.java
@@ -0,0 +1,12 @@
+package reporting;
+
+/**
+ * Created by nanjiang on 19/11/17.
+ */
+public class SortingAlgorithmReport {
+    long elementNumber;
+    long elementTraverseCount;
+    long elementTraverseTime;
+    long comparisonCount;
+    long comparisonTime;
+}
