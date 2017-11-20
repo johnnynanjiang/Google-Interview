e2d669df825adb3f3bf56991139398845c677d2c
Nan Jiang <johnny.nan.jiang@gmail.com>
2017-11-20 17:16:21 +1100

    add Gradle and Spring support

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,15 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import sorting.InsertionSort;
+
+/**
+ * Created by nanjiang on 20/11/17.
+ */
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public InsertionSort getInsertionSort() {
+        return new InsertionSort();
+    }
+}
---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,13 @@
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+/**
+ * Created by nanjiang on 20/11/17.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class, loader = AnnotationConfigContextLoader.class)
+public class BaseTest {
+}
