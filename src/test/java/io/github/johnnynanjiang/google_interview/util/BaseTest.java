c047a219bb7b55b412f46d5b0fc9d6a92a6ce716
Nan Jiang <johnny.nan.jiang@gmail.com>
2017-11-28 19:52:00 +1100

    mitigate warning messages in console

--- src/test/java/io/github/johnnynanjiang/google_interview/util/BaseTest.java
+++ src/test/java/io/github/johnnynanjiang/google_interview/util/BaseTest.java
@@ -3,8 +3,12 @@ package io.github.johnnynanjiang.google_interview.util;
 import io.github.johnnynanjiang.google_interview.util.AppConfig;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
 /**
  * Created by nanjiang on 20/11/17.
@@ -12,5 +16,6 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class, loader = AnnotationConfigContextLoader.class)
+@TestExecutionListeners(listeners = {DependencyInjectionTestExecutionListener.class})
 public abstract class BaseTest {
 }
