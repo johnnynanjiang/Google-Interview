ee8fe22e702f98d2bdc5d9f60014cc3654a8a9b1
Nan Jiang <johnny.nan.jiang@gmail.com>
2017-12-01 17:39:48 +1100

    refactoring

--- src/main/java/io/github/johnnynanjiang/google_interview/aspect/AlgorithmPerformanceAspect.java
+++ src/main/java/io/github/johnnynanjiang/google_interview/aspect/AlgorithmPerformanceAspect.java
@@ -1,9 +1,14 @@
 package io.github.johnnynanjiang.google_interview.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.JoinPoint;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 /**
  * Created by nanjiang on 27/11/17.
@@ -12,11 +17,29 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class AlgorithmPerformanceAspect {
-    @Before("execution(* io.github.johnnynanjiang.google_interview.sorting..*(..))")
+    @After("execution(* io.github.johnnynanjiang.google_interview.sorting..*(..))")
     protected void logAfter(JoinPoint joinPoint) {
+        printMessage("After", joinPoint);
+    }
+
+    //@Around("execution(* io.github.johnnynanjiang.google_interview.sorting..*(..))")
+    public void userAdvice(ProceedingJoinPoint joinPoint) throws Throwable{
+        printMessage("Around before ", joinPoint, "timestamp:" + new Date());
+        joinPoint.proceed();
+        printMessage("Around after ", joinPoint, "timestamp:" + new Date());
+    }
+
+    private void printMessage(String pointcut, JoinPoint joinPoint) {
+        printMessage(pointcut, joinPoint, "");
+    }
+
+    private void printMessage(String pointcut, JoinPoint joinPoint, String message) {
         System.out.println(
-                String.format("*** logAfter() is running, %s",
-                        joinPoint.getSignature().getName())
+                String.format("*** @%s: %s.%s(), %s",
+                        pointcut,
+                        joinPoint.getSignature().getDeclaringTypeName(),
+                        joinPoint.getSignature().getName(),
+                        message)
         );
     }
 }
--- src/test/java/io/github/johnnynanjiang/google_interview/util/BaseTest.java
+++ src/test/java/io/github/johnnynanjiang/google_interview/util/BaseTest.java
@@ -1,14 +1,11 @@
 package io.github.johnnynanjiang.google_interview.util;
 
-import io.github.johnnynanjiang.google_interview.util.AppConfig;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
 /**
  * Created by nanjiang on 20/11/17.
